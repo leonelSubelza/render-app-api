e7161f280e087a500026d888ff1bdaa5af6b6a05
leonelSubelza <subelza150@gmail.com>
2023-11-03 16:37:14 -0300

    Initial commit

---
+++ src/test/java/com/javawhizz/renderappapi/RenderAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javawhizz.renderappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
